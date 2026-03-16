23421c42ff71a12d31eaf086b126332b6c35fe87
Ksenia Bespalova <bespalovaksenya9@gmail.com>
2026-03-16 14:31:36 +0300

    main

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,12 +1,5 @@
 public class BonusMilesService {
     public int calculate(int price) {
-        int bonus = 20;
-        int miles;
-        if (price >= 1000) {
-            miles = price / bonus;
-        } else {
-            miles = 0;
-        }
-        return miles;
+        return price / 20;
     }
 }
